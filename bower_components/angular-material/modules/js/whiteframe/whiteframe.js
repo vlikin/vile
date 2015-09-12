bd7bc09eefd21baf96c480f29e60dfaf5beca9c3
Viktor Likin <viktor.likin@gmail.com>
2015-09-12 21:44:31 +0300

    Third party libraries.

---
+++ bower_components/angular-material/modules/js/whiteframe/whiteframe.js
@@ -0,0 +1,16 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.11.0-master-316b4c8
+ */
+(function( window, angular, undefined ){
+"use strict";
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+
+})(window, window.angular);
